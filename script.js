9cc02577814b31be03bc554a1a1f95bbff8d3601
Volatile <95543040+DarkShadowOfficial@users.noreply.github.com>
2024-10-16 11:26:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function For(action, start = 0, end, step = 1) {
+    for (let i = start; i < end; i += step) {
+        action(i);
+    }
+}
+function Iter(action, object) {
+    if (TYPE(object) == "object") {
+        for (let i of Object.getOwnPropertyNames(object)) {
+            action(i);
+        }
+    } else if (TYPE(object) == "array") {
+        object.forEach(x => {
+            action(x);
+        })
+    }
+}
+function While(action, statement) {
+    while (statement) {
+        action();
+    }
+}
+function If(statement, action) {
+    if (statement) {
+        action();
+    }
+}
+function IfElse(statement, action, alt_action) {
+    if (statement) {
+        action();
+    } else {
+        alt_action();
+    }
+}
+function IfChain(statements, actions, alt_action) {
+    for (let i = 0; i < statements.length; i++) {
+        if (statements[i]) {
+            actions[i]();
+            return;
+        }
+    }
+    alt_action();
+}
+Iter(i => {globalThis[i] = Math[i]}, Math)
+const XOR = (a, b) => (a || b) && !(a && b);
+const OR = (a, b) => a || b;
+const AND = (a, b) => a && b;
+const NOT = a => !a;
+const NOR = (a, b)  => !OR(a, b);
+const TYPE = a => a.push ? "array" : typeof a;
