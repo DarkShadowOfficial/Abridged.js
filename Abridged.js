c0b13ccf1c2199c30e0db0ec9b5444bb36f2cc29
Volatile <95543040+DarkShadowOfficial@users.noreply.github.com>
2024-10-18 08:43:09 -0500

    Update Abridged.js

--- Abridged.js
+++ Abridged.js
@@ -125,3 +125,29 @@ const sort = (arr) => {
   }
   return [...sort(leftArr), pivot, ...sort(rightArr)];
 };
+function search(array, target) {
+  // Code from my Jump Search Github Repo
+  function linear(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == target) return i;
+    }
+    return -1;
+  }
+  let buckets = [];
+  let arr = [...array];
+  for (let i = -1; i <= sqrt(arr.length); i++) {
+    buckets.push(arr.splice(0, sqrt(arr.length)));
+  }
+  buckets.push(arr);
+  let current;
+  let offset = 0;
+  for (let i = 0; i < buckets.length; i++) {
+    current = buckets[i].length - 1;
+    if (buckets[i][buckets[i].length - 1] == target) return current + offset;
+    else if (buckets[i][buckets[i].length - 1] > target) {
+      return linear(buckets[i], target) + offset;
+    }
+    offset += buckets[i].length - 1;
+  }
+  return -1;
+}
